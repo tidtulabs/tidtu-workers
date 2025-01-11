344a479071b3a9cc306966422e9afc6c788a2bab
ngtuonghy <ngtuonghy@gmail.com>
2025-01-11 20:16:30 +0700

    add some thing

--- apps/worker-cache/src/index.ts
+++ apps/worker-cache/src/index.ts
@@ -1,19 +1,8 @@
 import { Hono } from "hono";
 import router from "./routes";
 
-interface Env {
-	CACHE_TIDTU: KVNamespace;
-}
-const app = new Hono<{ Bindings: Env }>();
-
+const app = new Hono();
 app.route("/api/v1", router);
-
-// app.get("/", async (c) => {
-// 	const v = await c.env.CACHE_TIDTU.get("examList:frequency");
-//   // console.log(v);
-// 	return c.text(v as string);
-// });
-
 
 export default app;
 
