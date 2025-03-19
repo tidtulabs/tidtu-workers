68a597e5fd7417dd0dfdd446107be203f2867a96
ngtuonghy <ngtuonghy@gmail.com>
2025-03-19 11:17:14 +0700

    fix: disable ratelinit

--- apps/worker-download/src/index.ts
+++ apps/worker-download/src/index.ts
@@ -15,27 +15,27 @@ app.use("*", async (c, next) => {
 	return corsMiddlewareHandler(c, next);
 });
 
-app.use("*", async (c, next) => {
-	const allGetCookie = getCookie(c);
-	let deviceId = allGetCookie.deviceId;
-	if (!deviceId) {
-		console.log("No deviceId");
-		deviceId = crypto.randomUUID(); 
-		setCookie(c, "deviceId", deviceId, {
-			secure: true,
-      httpOnly: true,
-			path: "/",
-			sameSite: "Lax", 
-		});
-	}
-
-	const { success } = await c.env.MY_RATE_LIMITER.limit({ key: deviceId }); // key can be any string of your choosing
-	if (!success) {
-		return response.limitExceeded(c, "Rate limit exceeded");
-	} else {
-		await next();
-	}
-});
+// app.use("*", async (c, next) => {
+// 	const allGetCookie = getCookie(c);
+// 	let deviceId = allGetCookie.deviceId;
+// 	if (!deviceId) {
+// 		console.log("No deviceId");
+// 		deviceId = crypto.randomUUID(); 
+// 		setCookie(c, "deviceId", deviceId, {
+// 			secure: true,
+//       httpOnly: true,
+// 			path: "/",
+// 			sameSite: "Lax", 
+// 		});
+// 	}
+//
+// 	const { success } = await c.env.MY_RATE_LIMITER.limit({ key: deviceId }); // key can be any string of your choosing
+// 	if (!success) {
+// 		return response.limitExceeded(c, "Rate limit exceeded");
+// 	} else {
+// 		await next();
+// 	}
+// });
 
 app.route("/api/v1", router);
 
